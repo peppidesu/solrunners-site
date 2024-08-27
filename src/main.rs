497e3e7d6b1408ed23b06e92330ed26b8b1d889b
peppidesu <bakker.pepijn@gmail.com>
2024-08-27 17:13:09 +0200

    grass compile hook, gitignore on .css files

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,13 @@
 
 use solrunners_site::prelude::*;
 use solrunners_site::routes;
+use solrunners_site::scss::compile_all_scss;
 
 
 /// Builds the Rocket application
 #[launch]
-fn rocket() -> _ {
+fn rocket() -> _ {    
+    compile_all_scss();
     rocket::build()
         // Mount the main router
         .mount_router("/", routes::router())
